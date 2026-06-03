729b9eeef5d131423e526821de97424b27a96e81
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-03 14:07:52 +0200

    solve: module02, Discount calculator

--- src/main/java/module02/Exercise4_DiscountCalculator.java
+++ src/main/java/module02/Exercise4_DiscountCalculator.java
@@ -5,7 +5,16 @@ public class Exercise4_DiscountCalculator {
     public static void main(String[] args) {
         double price = 1200;
 
-        // Write your code below
+        if (price >= 1000) {
+            int discount = 20;
+            double finalPrice = price - (price * discount / 100);
+            System.out.printf("Discount: %d%%, Final price: R%.2f%n", discount, finalPrice);
+
+        } else if (price >= 500) {
+            int discount = 10;
+            double finalPrice = price - (price * discount / 100);
+            System.out.printf("Discount: %d%%, Final price: R%.2f%n", discount, finalPrice);
+        }else System.out.printf("Discount: %d%%, Final price: R%.2f%n", 0, price);
 
     }
 }
