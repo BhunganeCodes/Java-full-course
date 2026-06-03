2fb2676020c9d4113b244f99c464f0831d7198a1
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-03 14:15:49 +0200

    solve: module02, Weather advisor

--- src/main/java/module02/Exercise5_WeatherAdvisor.java
+++ src/main/java/module02/Exercise5_WeatherAdvisor.java
@@ -5,7 +5,19 @@ public class Exercise5_WeatherAdvisor {
     public static void main(String[] args) {
         int temperature = 35;
 
-        // Write your code below
+        if (temperature >= 30) {
+            String advice = "It's hot!";
+            System.out.printf("Temperature: %d°C, Advice: %s", temperature, advice);
+        }
+        else if (temperature >= 15) {
+            String advice = "It's mild.";
+            System.out.printf("Temperature: %d°C, Advice: %s", temperature, advice);
+        }
+        else {
+            String advice = "It's cold.";
+            System.out.printf("Temperature: %d°C, Advice: %s", temperature, advice);
+        }
+
 
     }
 }
