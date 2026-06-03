252321b7d29423710246c8f4e20e1b474281af98
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-03 13:54:43 +0200

    solve: module02, temperature alert

--- src/main/java/module02/Exercise1_TemperatureAlert.java
+++ src/main/java/module02/Exercise1_TemperatureAlert.java
@@ -5,7 +5,9 @@ public class Exercise1_TemperatureAlert {
     public static void main(String[] args) {
         int temperature = 35;
 
-        // Write your code below
+        if (temperature >= 35) {
+            System.out.println("It's hot!");
+        } else System.out.println("It's not hot.");
 
     }
 }
