a0f14974f87976917e8506685739785dc262ed4e
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-06-04 11:50:34 +0200

    solve: module03, Loop exercises

--- src/main/java/module03/LoopExercises.java
+++ src/main/java/module03/LoopExercises.java
@@ -1,5 +1,7 @@
 package module03;
 
+import java.util.Objects;
+
 public class LoopExercises {
 
     // Exercise 1
@@ -15,35 +17,67 @@ public class LoopExercises {
     // Exercise 2
     public static int exercise2_pinValidator(int[] pins) {
         int result = 1;
-        int idx = 0;
+        int idx = -1;
+        int attempts = 0;
 
         do {
-            if (pins[idx] != 1234) {
-                idx++;
-                result++;
-            }
+            attempts++;
+            idx++;
         } while (pins[idx] != 1234);
 
-        return result;
+        return attempts;
     }
 
     // Exercise 3
     public static int exercise3_sumUntilZero(int[] numbers) {
-        return 0;
+        int result = 0;
+        for (int num : numbers) {
+            if (num == 0) {
+                break;
+            }
+            result += num;
+        }
+        return result;
     }
 
     // Exercise 4
     public static String exercise4_numberPrinter(int n) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < n; i++) {
+            if (i % 5 == 0) continue;
+            if (i == n - 1) {
+                sb.append(i);
+            } else {
+                sb.append(i + ", ");
+            }
+        }
+        return sb.toString().trim();
     }
 
     // Exercise 5
     public static String exercise5_passwordChecker(String[] attempts) {
-        return null;
+        int maxAttempts = 3;
+        int attemptNum = 0;
+        String password = "secret";
+
+         for (String w : attempts) {
+             attemptNum++;
+             if (w.toLowerCase() == password) return "Access granted!";
+             if (attemptNum == maxAttempts) return "Access denied";
+
+         }
+        return "Access denied";
     }
 
     // Exercise 6
     public static String exercise6_evenNumbers(int n) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) continue;
+            if (i % 2 == 0 && i == n) sb.append(i);
+            else if (i % 2 == 0) sb.append(i + ", ");
+        }
+        return sb.toString();
     }
 }
